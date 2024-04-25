369fe93a84d538e776570b8c7a9c75f25f9cab78
Julien Tilman <julien.tilman@outlook.com>
2024-04-25 16:51:26 +0200

    feat add navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,45 @@
 import { useState } from "react";
+import {
+  FaHome,
+  FaChartLine,
+  FaStar,
+  FaLightbulb,
+  FaMoon,
+} from "react-icons/fa";
 
 export default function Navbar() {
+  const [darkMode, setDarkMode] = useState(false);
 
+  const toggleDarkMode = () => {
+    setDarkMode((prevMode) => !prevMode);
+  };
 
-    return (
-		<>
-			<div className="navbar">
-
-				<p>Navbar</p>
-			</div>
-			</>
-		);
+  return (
+    <>
+      <div className={`navbar ${darkMode ? "navbar--dark" : "navbar--light"}`}>
+        <nav className="navbar__icons">
+          <ul>
+            <li>
+              <a href="">
+                <FaHome />
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <FaChartLine />
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <FaStar />
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <button onClick={toggleDarkMode}>
+          {darkMode ? <FaLightbulb /> : <FaMoon />}
+        </button>
+      </div>
+    </>
+  );
 }
