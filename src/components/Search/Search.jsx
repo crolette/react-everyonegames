758cb257ff654a768a7b5cb812b28e7b922ee3b3
Julien Tilman <julien.tilman@outlook.com>
2024-04-26 15:53:53 +0200

    feat add search page

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,12 +1,24 @@
-import { useState } from 'react';
+import { useState } from "react";
+import { FaSearch } from "react-icons/fa";
 
 export default function Search() {
-	return (
-		<>
-			<div className="search">
-
-			<p>Search</p>
-			</div>
-		</>
-	);
+  function handleChange(event) {
+    // console.log(event.target.value);
+    if (event.target.value.length > 3) {
+      //   callAPI();
+      console.log("API called");
+    }
+  }
+  return (
+    <>
+      <div className="search">
+        <FaSearch className="search-icon" />
+        <input
+          type="text"
+          placeholder="Search for your favorite game"
+          onChange={handleChange}
+        />
+      </div>
+    </>
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,56 +1,60 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import App from './pages/App';
-import Game from './pages/Game';
-import ErrorPage from './pages/ErrorPage.jsx';
-import './index.scss';
-import BestOf from './components/GamesList/BestOf.jsx';
-import GamesList from './components/GamesList/GamesList.jsx';
-import NewReleases from './components/GamesList/NewReleases.jsx';
-import NextReleases from './components/GamesList/NextReleases.jsx';
-import GamePage from './components/Game/GamePage';
-import GamesByGenre from './components/GamesList/GamesByGenre.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./pages/App";
+import Game from "./pages/Game";
+import ErrorPage from "./pages/ErrorPage.jsx";
+import "./index.scss";
+import BestOf from "./components/GamesList/BestOf.jsx";
+import GamesList from "./components/GamesList/GamesList.jsx";
+import NewReleases from "./components/GamesList/NewReleases.jsx";
+import NextReleases from "./components/GamesList/NextReleases.jsx";
+import GamePage from "./components/Game/GamePage";
+import GamesByGenre from "./components/GamesList/GamesByGenre.jsx";
+import SearchPage from "./components/Search/SearchPage.jsx";
 
 console.log(import.meta.env.VITE_API_KEY);
 
 const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <App />,
-		errorElement: <ErrorPage />,
-		children: [
-			{
-				path: '',
-				element: <GamesList />,
-			},
-			{
-				path: '/best-of',
-				element: <BestOf />,
-			},
-			{
-				path: '/new-releases',
-				element: <NewReleases />,
-			},
-			{
-				path: '/next-releases',
-				element: <NextReleases />,
-			},
-			{
-				path: '/games/:genreId/:genreSlug',
-				element: <GamesByGenre />,
-			},
-		],
-	},
-	{
-		path: '/game/:gameId',
-		element: <Game />,
-	},
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "",
+        element: <GamesList />,
+      },
+      {
+        path: "/best-of",
+        element: <BestOf />,
+      },
+      {
+        path: "/new-releases",
+        element: <NewReleases />,
+      },
+      {
+        path: "/next-releases",
+        element: <NextReleases />,
+      },
+      {
+        path: "/games/:genreId/:genreSlug",
+        element: <GamesByGenre />,
+      },
+      {
+        path: "/search-page",
+        element: <SearchPage />,
+      },
+    ],
+  },
+  {
+    path: "/game/:gameId",
+    element: <Game />,
+  },
 ]);
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-	<React.StrictMode>
-		<RouterProvider router={router} />
-	</React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 );
